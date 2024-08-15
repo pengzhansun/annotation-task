8458596d1cb861f930150015e26444c5585f684c
pengzhansun <pengzhansun6@gmail.com>
2024-08-15 18:55:49 +0800

    Updated annotation templates and fixed typos in several HTML files

---
+++ make_htmls_csv.py
@@ -0,0 +1,25 @@
+import os
+import csv
+
+# Define the folder path
+folder_path = '/home/pengzhan/annotation-task/html_anns'
+url_prefix = "https://pengzhansun.github.io/annotation-task/html_anns"
+
+# Get all files in the folder
+file_names = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+# Combine the URL prefix with each file name using os.path.join
+urls = [os.path.join(url_prefix, file_name) for file_name in file_names]
+
+# Get the first 20 URLs
+urls = urls[:20]
+
+# Save the URLs to a CSV file
+csv_file_path = 'paco_sentences_for_test.csv'  # You can specify the path where you want to save the CSV file
+
+with open(csv_file_path, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    for url in urls:
+        writer.writerow([url])  # Write each URL into column A
+
+# print(f"CSV file with URLs has been saved to {csv_file_path}")
